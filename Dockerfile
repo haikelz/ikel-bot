bb9f0b236adb8655b75495b107def0fddc9431c2
Haikel Ilham Hakim <haikel.2017th@gmail.com>
2025-09-05 10:10:01 +0700

    fix(docker): make the docker build works

--- Dockerfile
+++ Dockerfile
@@ -1,5 +1,13 @@
 FROM golang:1.23.4-alpine AS builder
 
+# Install Tesseract OCR and development libraries
+RUN apk add --no-cache \
+    tesseract-ocr \
+    tesseract-ocr-dev \
+    gcc \
+    musl-dev \
+    build-base
+
 WORKDIR /app
 
 COPY go.mod go.sum ./
--- pkg/utils/env.go
+++ pkg/utils/env.go
@@ -2,26 +2,13 @@ package utils
 
 import (
 	"katou-megumi/pkg/configs"
+	"katou-megumi/pkg/entities"
 	"log"
 
 	"github.com/spf13/viper"
 )
 
-type EnvVariables struct {
-	DISCORD_TOKEN        string
-	REMOVE_BG_API_KEY    string
-	REMOVE_BG_API_URL    string
-	JOKES_API_URL        string
-	ANIME_QUOTE_API_URL  string
-	DISTRO_INFO_API_URL  string
-	DOA_API_URL          string
-	QURAN_API_URL        string
-	IMAGE_API_URL        string
-	GEMINI_API_KEY       string
-	ASMAUL_HUSNA_API_URL string
-}
-
-func Env() EnvVariables {
+func Env() entities.EnvVariables {
 	v := configs.NewViper()
 
 	_ = v.BindEnv("DATABASE_URL")
@@ -43,7 +30,7 @@ func Env() EnvVariables {
 		}
 	}
 
-	return EnvVariables{
+	return entities.EnvVariables{
 		DISCORD_TOKEN:        v.GetString("DISCORD_TOKEN"),
 		REMOVE_BG_API_KEY:    v.GetString("REMOVE_BG_API_KEY"),
 		REMOVE_BG_API_URL:    v.GetString("REMOVE_BG_API_URL"),
